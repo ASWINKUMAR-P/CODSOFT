d83ee80c699d1d13e55fa66b28b79fd3b0a8897a
aswinkumar-p <aswi20113.cs@rmkec.ac.in>
2023-07-24 17:31:01 +0530

    Task 1 completed

--- Task1-ToDoList/main.py
+++ Task1-ToDoList/main.py
@@ -1,18 +1,22 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk,messagebox
 from tkinter.constants import BOTH, LEFT, RIGHT, VERTICAL, Y
 import sqlite3
+from PIL import Image, ImageTk
+
 
 def add(new_task):
     query = "insert into task(task,status) values('"+new_task+"','pending')"
     cur.execute(query)
     mydb.commit()
+    messagebox.showinfo("Success","Task added successfully")
     createMainFrame(main_frame,cur)
 
 def remove(id):
     query = "update task set status='done' where id='"+str(id)+"'"
     cur.execute(query)
     mydb.commit()
+    messagebox.showinfo("Success","Task removed successfully")
     createMainFrame(main_frame,cur)
 
 def createMainFrame(main_frame,cur):
@@ -20,33 +24,31 @@ def createMainFrame(main_frame,cur):
     for widget in main_frame.winfo_children():
         widget.destroy()
         
-    head = tk.Label(main_frame,text="Task Manager",font=("Impact", 20),background="blue",anchor="center",width=40).grid(row=0,column=0,columnspan=2)
+    tk.Label(main_frame,text="Task Manager",font=("Impact", 25),background="purple",anchor="center",width=30).grid(row=0,column=0,columnspan=2)
     tk.Label(main_frame,text="").grid(row=1,column=0,columnspan=2)
-    task = tk.Label(main_frame,text="Pending Task",font=("Impact", 18),anchor="w").grid(row=2,column=0,sticky="w",padx=20)
+    tk.Label(main_frame,text="Add a task",font=("Times New Roman", 18,"bold")).grid(row=2,column=0,sticky="w",padx=20)
     tk.Label(main_frame,text="").grid(row=3,column=0,columnspan=2)
+    tk.Label(main_frame,text="Enter the task:-",font=("Times New Roman", 18)).grid(row=4,column=0,sticky="w",padx=20)
+    new_task = tk.Entry(main_frame,width=20,font=("Times New Roman",16))
+    new_task.grid(row=4,column=1,sticky="w",padx=20)
+    tk.Label(main_frame,text="").grid(row=5,column=0,columnspan=2)
+    tk.Button(main_frame,text="Add",font=("Times New Roman", 18),width=5,background="purple",command=lambda:add(new_task.get())).grid(row=6,column=0,sticky="w",padx=20)
+    tk.Label(main_frame,text="").grid(row=7,column=0,columnspan=2)
+    tk.Label(main_frame,text="Pending Task" ,font=("Times New Roman", 18,"bold"),anchor="w").grid(row=8,column=0,sticky="w",padx=20)
+    tk.Label(main_frame,text="").grid(row=9,column=0,columnspan=2)
 
     query = "select * from task where status='pending'"
     cur.execute(query)
     tasks = cur.fetchall()
-
-    rowcount = 4
+    rowcount = 10
     rowindex = 1
 
     for task in tasks:
         tk.Label(main_frame,text=str(rowindex)+". "+task[1],font=("Times New Roman", 18)).grid(row=rowcount,column=0,sticky="w",padx=20)
-        button=tk.Button(main_frame,text="Done",font=("Times New Roman", 18),width=7,background="blue",command=lambda:remove(task[0]))
-        button.grid(row=rowcount,column=1,sticky="w",padx=20)
-        tk.Label(main_frame,text="").grid(row=rowcount+1,column=0,columnspan=2)
-        rowcount+=2
+        delete_button=tk.Button(main_frame,text="Remove",font=("Times New Roman", 18),command=lambda id=task[0]:remove(id))
+        delete_button.grid(row=rowcount,column=1)
+        rowcount+=1
         rowindex+=1
-
-    tk.Label(main_frame,text="Add a task",font=("Impact", 18),anchor="w").grid(row=rowcount,column=0,columnspan=2,sticky="w",padx=20)
-    tk.Label(main_frame,text="").grid(row=rowcount+1,column=0,columnspan=2)
-    tk.Label(main_frame,text="Enter the task:-",font=("Times New Roman", 18)).grid(row=rowcount+2,column=0,sticky="w",padx=20)
-    new_task = tk.Entry(main_frame,width=16,font=("Times New Roman",16))
-    new_task.grid(row=rowcount+2,column=1,sticky="w",padx=20)
-    tk.Label(main_frame,text="").grid(row=rowcount+3,column=0,columnspan=2)
-    tk.Button(main_frame,text="Add",font=("Times New Roman", 18),width=5,background="blue",command=lambda:add(new_task.get())).grid(row=rowcount+4,column=0,sticky="w",padx=20)
 
 home=tk.Tk()
 home.geometry('500x500')
@@ -78,4 +80,4 @@ my_canvas.create_window((0,0),window=main_frame,anchor="nw")
 createMainFrame(main_frame,cur)
 
 home.mainloop()
-    
+mydb.close()
