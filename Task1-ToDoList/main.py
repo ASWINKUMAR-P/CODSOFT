67d5545ec0b25c3c1a93b693c006fd09b3294868
aswinkumar-p <aswi20113.cs@rmkec.ac.in>
2023-07-24 05:41:53 +0530

    Back end logic also completed

--- Task1-ToDoList/main.py
+++ Task1-ToDoList/main.py
@@ -3,11 +3,50 @@ from tkinter import ttk
 from tkinter.constants import BOTH, LEFT, RIGHT, VERTICAL, Y
 import sqlite3
 
-def add(task):
-    pass
+def add(new_task):
+    query = "insert into task(task,status) values('"+new_task+"','pending')"
+    cur.execute(query)
+    mydb.commit()
+    createMainFrame(main_frame,cur)
 
 def remove(id):
-    pass
+    query = "update task set status='done' where id='"+str(id)+"'"
+    cur.execute(query)
+    mydb.commit()
+    createMainFrame(main_frame,cur)
+
+def createMainFrame(main_frame,cur):
+
+    for widget in main_frame.winfo_children():
+        widget.destroy()
+        
+    head = tk.Label(main_frame,text="Task Manager",font=("Impact", 20),background="blue",anchor="center",width=40).grid(row=0,column=0,columnspan=2)
+    tk.Label(main_frame,text="").grid(row=1,column=0,columnspan=2)
+    task = tk.Label(main_frame,text="Pending Task",font=("Impact", 18),anchor="w").grid(row=2,column=0,sticky="w",padx=20)
+    tk.Label(main_frame,text="").grid(row=3,column=0,columnspan=2)
+
+    query = "select * from task where status='pending'"
+    cur.execute(query)
+    tasks = cur.fetchall()
+
+    rowcount = 4
+    rowindex = 1
+
+    for task in tasks:
+        tk.Label(main_frame,text=str(rowindex)+". "+task[1],font=("Times New Roman", 18)).grid(row=rowcount,column=0,sticky="w",padx=20)
+        button=tk.Button(main_frame,text="Done",font=("Times New Roman", 18),width=7,background="blue",command=lambda:remove(task[0]))
+        button.grid(row=rowcount,column=1,sticky="w",padx=20)
+        tk.Label(main_frame,text="").grid(row=rowcount+1,column=0,columnspan=2)
+        rowcount+=2
+        rowindex+=1
+
+    tk.Label(main_frame,text="Add a task",font=("Impact", 18),anchor="w").grid(row=rowcount,column=0,columnspan=2,sticky="w",padx=20)
+    tk.Label(main_frame,text="").grid(row=rowcount+1,column=0,columnspan=2)
+    tk.Label(main_frame,text="Enter the task:-",font=("Times New Roman", 18)).grid(row=rowcount+2,column=0,sticky="w",padx=20)
+    new_task = tk.Entry(main_frame,width=16,font=("Times New Roman",16))
+    new_task.grid(row=rowcount+2,column=1,sticky="w",padx=20)
+    tk.Label(main_frame,text="").grid(row=rowcount+3,column=0,columnspan=2)
+    tk.Button(main_frame,text="Add",font=("Times New Roman", 18),width=5,background="blue",command=lambda:add(new_task.get())).grid(row=rowcount+4,column=0,sticky="w",padx=20)
 
 home=tk.Tk()
 home.geometry('500x500')
@@ -36,30 +75,7 @@ my_canvas.bind('<Configure>',lambda e:my_canvas.configure(scrollregion=my_canvas
 main_frame=ttk.Frame(my_canvas)
 my_canvas.create_window((0,0),window=main_frame,anchor="nw")
 
-head = tk.Label(main_frame,text="Task Manager",font=("Impact", 20),background="blue",anchor="center",width=40).grid(row=0,column=0,columnspan=2)
-tk.Label(main_frame,text="").grid(row=1,column=0,columnspan=2)
-task = tk.Label(main_frame,text="Pending Task",font=("Impact", 18),anchor="w").grid(row=2,column=0,sticky="w",padx=20)
-tk.Label(main_frame,text="").grid(row=3,column=0,columnspan=2)
+createMainFrame(main_frame,cur)
 
-query = "select * from task where status='pending'"
-cur.execute(query)
-tasks = cur.fetchall()
-
-rowcount = 4
-rowindex = 1
-
-for task in tasks:
-    tk.Label(main_frame,text=str(rowindex)+". "+task[1],font=("Times New Roman", 18)).grid(row=rowcount,column=0,sticky="w",padx=20)
-    tk.Button(main_frame,text="Done",font=("Times New Roman", 18),width=7,background="blue",command=lambda:remove(task[0])).grid(row=rowcount,column=1,sticky="w",padx=20)
-    tk.Label(main_frame,text="").grid(row=rowcount+1,column=0,columnspan=2)
-    rowcount+=2
-    rowindex+=1
-
-tk.Label(main_frame,text="Add a task",font=("Impact", 18),anchor="w").grid(row=rowcount,column=0,columnspan=2,sticky="w",padx=20)
-tk.Label(main_frame,text="").grid(row=rowcount+1,column=0,columnspan=2)
-tk.Label(main_frame,text="Enter the task:-",font=("Times New Roman", 18)).grid(row=rowcount+2,column=0,sticky="w",padx=20)
-task = tk.Entry(main_frame,width=16,font=("Times New Roman",16)).grid(row=rowcount+2,column=1,sticky="w",padx=20)
-tk.Label(main_frame,text="").grid(row=rowcount+3,column=0,columnspan=2)
-tk.Button(main_frame,text="Add",font=("Times New Roman", 18),width=5,background="blue",command=lambda:add(task)).grid(row=rowcount+4,column=0,sticky="w",padx=20)
 home.mainloop()
     
