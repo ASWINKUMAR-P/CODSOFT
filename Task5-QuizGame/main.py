2f6e1c111fd9876b2c19e9eda3ac5467289143bd
aswinkumar-p <aswi20113.cs@rmkec.ac.in>
2023-07-31 17:02:37 +0530

    Task 5 completed

---
+++ Task5-QuizGame/main.py
@@ -0,0 +1,132 @@
+from questions import questions
+from tkinter import *
+from tkinter import messagebox
+import random
+
+def resultpage(window):
+    window.destroy()
+    window = Tk()
+    window.title("Quiz Game") 
+    window.resizable(False, False)
+    window.geometry("600x500")
+    global score
+    Label(window, text="").grid(row=0, column=0)
+    Label(window, text="Python Quiz Game", font=("Times New Roman", 20,"bold underline"), borderwidth=3, anchor="center",width=35).grid(row=1, column=0)
+    Label(window, text="").grid(row=2, column=0)
+    Label(window, text="Result:", font=(" Times New Roman", 20, "bold")).grid(row=3, column=0)
+    Label(window, text="").grid(row=4, column=0)
+    Label(window, text=" Your Score: " + str(score) + "/5", font=("Times New Roman", 20, "bold")).grid(row=5, column=0)
+    Label(window, text="").grid(row=6, column=0)
+    Button(window, text="Play Again", font=("Times New Roman", 15), command=lambda: home()).grid(row=7, column=0)
+
+
+
+def checkanswer(window, q, answer, Options, button, qnum):
+    if answer.get() == "None":
+        messagebox.showerror("Error", "Please select an option")
+        return
+    
+    correct_answer = q["answer"]
+    selected_answer = answer.get()
+    print(selected_answer)
+
+    for i in range(4):
+        if correct_answer == q["options"][i]:
+            correct_option = i+1
+            break
+    
+    for i in range(4):
+        if selected_answer == q["options"][i]:
+            selected_option = i+1
+            break
+    
+    if correct_option == selected_option:
+        Options[correct_option-1].config(text=str(correct_option) + ". " + q["options"][correct_option-1] + " \u2713", fg="green")
+        global score
+        score += 1
+    else:
+        Options[correct_option-1].config(text=str(correct_option) + ". " + q["options"][correct_option-1] + " \u2713", fg="green")
+        Options[selected_option-1].config(text=str(selected_option) + ". " + q["options"][selected_option-1] + " x", fg="red")
+    if qnum < 4:
+        button.config(text="Go to next Question", command=lambda: starttest(window, qnum+1))
+    else:
+        button.config(text="Go to Result Page", command=lambda: resultpage(window))
+
+
+def starttest(window, qnum):
+    try:
+        if window.winfo_exists() == True:
+            window.destroy()
+    except:
+        pass
+    
+    questpage = Tk()
+    questpage.title("Quiz Game") 
+    questpage.resizable(False, False)
+    questpage.geometry("600x500")
+    question = questions[qnum]
+    
+    Label(questpage, text="").grid(row=0, column=0, columnspan=2)
+    Label(questpage, text="Python Quiz Game", font=("Times New Roman", 20,"bold underline"), borderwidth=3, anchor="center").grid(row=1, column=0, columnspan=2)
+    Label(questpage, text="").grid(row=2, column=0, columnspan=2)
+    Label(questpage, text=" ").grid(row=3, column=0, sticky="w")
+    Label(questpage, text=question["question"], font=("Times New Roman", 15, "bold"), wraplength=598, justify="left", width=50, anchor="w").grid(row=3, column=1, sticky="w")
+    Label(questpage, text="").grid(row=4, column=0, columnspan=2)
+    Label(questpage, text=" ").grid(row=5, column=0, sticky="w")
+    Label(questpage, text="Options:", font=("Times New Roman", 15, "bold")).grid(row=5, column=1, sticky="w")
+    Label(questpage, text="").grid(row=6, column=0, columnspan=2)        
+    
+    answer = StringVar()
+    answer.set("None")
+    
+    Label(questpage, text=" ").grid(row=7, column=0)
+    Label(questpage, text=" ").grid(row=8, column=0)
+    Label(questpage, text=" ").grid(row=9, column=0)
+    Label(questpage, text=" ").grid(row=10, column=0)
+    
+    Options = []
+    for i in range(4):
+        Options.append(Radiobutton(questpage, text=str(i+1) + ". " + question["options"][i], font=("Times New Roman", 15), variable=answer, value=question["options"][i], wraplength=500, justify="left"))
+        Options[i].grid(row=7+i, column=1, sticky="w")        
+    
+    Label(questpage, text=" ").grid(row=11, column=0, columnspan=2)
+    button = Button(questpage, text="Submit", font=("Times New Roman", 15), command=lambda: checkanswer(questpage, question, answer, Options, button, qnum))
+    button.grid(row=12, column=0, columnspan=2)
+    
+    questpage.mainloop()
+
+
+score = 0
+
+def home():
+
+    global questions
+    questions = random.sample(questions, 5)
+    
+    window = Tk()
+    window.title("Quiz Game")
+    window.resizable(False, False)
+    
+    Label(window, text="").grid(row=0, column=0)
+    Label(window, text="Welcome to the Python Quiz Game!", font=("Times New Roman", 20, "bold underline"), borderwidth=3).grid(row=1, column=0, columnspan=2)
+    Label(window, text="").grid(row=2, column=0, columnspan=2)
+    Label(window, text=" ").grid(row=3, column=0)
+    Label(window, text="Instructions:", font=("Times New Roman", 15, "bold")).grid(row=3, column=1, sticky="w")
+    Label(window, text="").grid(row=4, column=0)
+    Label(window, text=" ").grid(row=5, column=0)
+    Label(window, text="1. There are 5 questions in this quiz.", font=("Times New Roman", 15)).grid(row=5, column=1, sticky="w")
+    Label(window, text=" ").grid(row=6, column=0)
+    Label(window, text="2. Each question has 4 options.", font=("Times New Roman", 15)).grid(row=6, column=1, sticky="w")
+    Label(window, text=" ").grid(row=7, column=0)
+    Label(window, text="3. You have to select one option out of the 4 options.", font=("Times New Roman", 15)).grid(row=7, column=1, sticky="w")
+    Label(window, text=" ").grid(row=8, column=0)
+    Label(window, text="4. You can't change your answer once you click submit button    ", font=("Times New Roman", 15)).grid(row=8, column=1, sticky="w")
+    Label(window, text=" ").grid(row=9, column=0)
+    Label(window, text="5. Your result will be displayed at the end of the test", font=("Times New Roman", 15)).grid(row=9, column=1, sticky="w")
+    Label(window, text="").grid(row=10, column=0, columnspan=2)
+    Button(window, text="Start Quiz", font=("Times New Roman", 15, "bold"), anchor="center", bg="blue", fg="white", command=lambda: starttest(window, 0)).grid(row=11, column=0, columnspan=2)
+    Label(window, text="").grid(row=12, column=0, columnspan=2)
+    
+    window.mainloop()
+
+home()
---
+++ Task5-QuizGame/questions.py
@@ -0,0 +1,167 @@
+questions=[
+    {
+        "id":1, 
+        "question":"Who invented python?", 
+        "options":[
+            "Guido van Rossum", 
+            "Dennis Ritchie", 
+            "Bjarne Stroustrop", 
+            "James Gosling"
+        ], 
+        "answer":"Guido van Rossum"
+    },
+    {
+        "id":2, 
+        "question":"Which of the following is not a keyword in python?", 
+        "options":[
+            "val", 
+            "raise", 
+            "try", 
+            "with"
+        ], 
+        "answer":"val"
+    },
+    {
+        "id":3, 
+        "question":"Which of the following is not a python data type?",
+        "options":[
+            "list", 
+            "tuple", 
+            "class", 
+            "dictionary"
+        ], 
+        "answer":"class"
+    },
+    {
+        "id":4,
+        "question":"What is the output of the following code?\n\n   print(2+3*5)",
+        "options":[
+            "25", 
+            "17", 
+            "13", 
+            "None of the above"
+        ], 
+        "answer":"17"
+    },
+    {
+        "id":5,
+        "question":"Which are the correct variable names in python?",
+        "options":[
+            "1var",
+            "var_1",
+            "var1$",
+            "@var1"
+        ], 
+        "answer":"var_1"
+    },
+    {
+        "id":6, 
+        "question":"Which of the following is the correct way to open a file named 'data.txt' in Python for reading?",
+        "options":[
+            "file = open('data.txt', 'r')", 
+            "file = open('data.txt')", 
+            "file = open('data.txt', 'w')", 
+            "file = open('data.txt', 'a')"
+            ],
+        "answer":"file = open('data.txt', 'r')"
+    },
+    {
+        "id":7,
+        "question":"Find the output of the following code:\n\n   my_list = [1, 2, 3, 4, 5]\n   result = [x ** 2 for x in my_list if x % 2 == 0]\n   print(result)",
+        "options":[
+            "It prints the squares of all the elements in my_list",
+            "It prints the squares of all the even elements in my_list",
+            "It prints the squares of all the odd elements in my_list",
+            "It prints the squares of all the elements in my_list except the first one"
+        ],
+        "answer":"It prints the squares of all the even elements in my_list"
+    },
+    {
+        "id":8,
+        "question":"How do you write a comment in Python?",
+        "options":[
+            "//This is a comment",
+            "#This is a comment",
+            "/*This is a comment*/",
+            "/*This is a comment"
+        ],
+        "answer":"#This is a comment"
+    },
+    {
+        "id":9,
+        "question":"What is the output of the following code?\n\n   x = 'Hello, World!'\n   print(x[7:12])",
+        "options":[
+            "World",
+            "World!",
+            "World!Hello",
+            "Hello"
+        ],
+        "answer":"World"
+    },
+    {
+        "id":10,
+        "question":"Which operator is used for exponentiation in Python?",
+        "options":[
+            "**",
+            "^",
+            "*",
+            "//"
+        ],
+        "answer":"**"
+    },
+    {
+        "id":11,
+        "question":"Which of the datatypes is not mutable in Python?",
+        "options":[
+            "list",
+            "tuple",
+            "dictionary",
+            "set"
+        ],
+        "answer":"tuple"
+    },
+    {
+        "id":12,
+        "question":"Which Python keyword is used to define a function?",
+        "options":[
+            "func",
+            "def",
+            "define",
+            "function"
+        ],
+        "answer":"def"
+    },
+    {
+        "id":13,
+        "question":"In Python, what is the purpose of the elif statement?",
+        "options":[
+            "It is used to terminate a loop.",
+            "It is used to handle exceptions.",
+            "It is used to define a function.",
+            "It is used to specify an alternative condition to check if the preceding if statement is false."
+        ],
+        "answer":"It is used to specify an alternative condition to check if the preceding if statement is false."
+    },
+    {
+        "id":14,
+        "question":"Which keyword is used to catch and handle exceptions in Python?",
+        "options":[
+            "try",
+            "except",
+            "catch",
+            "finally"
+        ],
+        "answer":"except"
+    },
+    {
+        "id":15,
+        "question":"What will happen if an exception occurs in the try block and there is no corresponding except block to handle it?",
+        "options":[
+            "The program will continue executing normally.",
+            "The program will terminate with an error message.",
+            "The program will wait for user input to handle the exception.",
+            "The program will enter an infinite loop."
+        ],
+        "answer":"The program will terminate with an error message."
+    }
+]
